090d807e33ecf211e383558672881f10554ee05a
Ozeias Souza <o.souzati@gmail.com>
2023-10-31 23:12:05 -0400

    Alterando cores

--- src/Constants/Colors.ts
+++ src/Constants/Colors.ts
@@ -1,9 +1,9 @@
 enum Colors {
-    RED     = '0xFF0000',
-    GREEN   = '0x00FF00',
-    BLUE    = '0x0000FF',
-    BLACK   = '0x000000',
-    WHITE   = '0xFFFFFF'
+    RED     = '#FF0000',
+    GREEN   = '#00FF00',
+    BLUE    = '#0000FF',
+    BLACK   = '#000000',
+    WHITE   = '#FFFFFF'
 }
 
 
